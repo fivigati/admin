2c6fc5725f9923da82cec6797b93122947144aea
Fivigati Nurhayati <fivigati@gmail.com>
2026-05-27 11:37:37 +0700

    Create api.js

---
+++ js/api.js
@@ -0,0 +1,43 @@
+const API_URL =
+'https://script.google.com/macros/s/AKfycbx3SamwedBeCTTJGfN3BXMr0CEePYNupPsbxspwGM4GzA2izbWy5we-qo_SBWASh51BRA/exec';
+
+async function apiRequest(payload) {
+
+  try {
+
+    const formData = new FormData();
+
+    formData.append(
+      'data',
+      JSON.stringify(payload)
+    );
+
+    const response = await fetch(API_URL, {
+
+      method: 'POST',
+
+      body: formData
+
+    });
+
+    const text =
+      await response.text();
+
+    console.log(text);
+
+    return JSON.parse(text);
+
+  } catch(err) {
+
+    console.error(err);
+
+    return {
+
+      success:false,
+      message:'Server error'
+
+    };
+
+  }
+
+}
